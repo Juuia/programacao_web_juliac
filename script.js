a9cc706c2e7bce7d7b33a60ed80d68dadea200c7
Juuia <juliaacarvalhoo@sempreceub.com>
2024-08-13 09:23:15 -0300

    java

---
+++ script.js
@@ -0,0 +1,9 @@
+let titulo = document.getElementById("titulo").value;
+let nome = document.getElementById("nome").value;
+
+console.log(titulo);
+console.log(nome);
+
+function submeter() {
+    alert("Estou na função submeter");
+}
